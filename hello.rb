6590b98c883aac44dcb2656ed31d60dc8428d141
Joshua <rodriguez.taco.joshua@gmail.com>
2022-09-12 15:18:28 +0400

    Ruby class leccion 1

---
+++ hello.rb
@@ -0,0 +1,19 @@
+class Song
+
+    def initialize(name, duration, artist)
+        @name = name
+        @artist = artist
+        @duration = duration
+    end
+
+    def getName
+        @name
+    end 
+end 
+
+# We can acces to a variable throught getters 
+ 
+
+song1 = Song.new("legends never die", 100, "wow")
+
+puts song1.name
